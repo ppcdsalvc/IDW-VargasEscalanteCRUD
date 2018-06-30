2657e82b26b92ce1e922d6c24a25bd0ea17ce711
arturoescalante <armiescalanteli@ittepic.edu.mx>
2018-06-30 12:14:12 -0600

    6- actualizacion por correo

--- funciones.js
+++ funciones.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
-
+//1-CONSULTA GENERAL
 function leer (){
-    
     al = Array();
     fs.readFile('alumnos.json', 'utf8', (err, data) => {
         if (err)
@@ -9,46 +8,150 @@ function leer (){
         var al = JSON.parse(data);
         al.forEach(element => {
             console.log(element);
+
             
         });
-        /*for (var i = 0; i < linea.length; i++) {
-            var line = linea[i];
-            console.log(line);
-        }*/
-      
-    });
-   // return linea;
+
+    });  
 
 }
-
-
-function BuscarElemento(posicion){
+//2-CONSULTA POR POSICION
+function leerXposicion(posicion){
     fs.readFile('alumnos.json', 'utf8', (err, data) => {
         if (err)
             throw err;
         var arreglo = JSON.parse(data);
-         var newArr = arreglo.filter(function(item){
-            return item.email === "Arturo@ittepic.edu.mx";
-        });
-        console.log("Filter results:",newArr);
-    }); 
-}
-
-
-
-function AgregaElemento(){
-    fs.readFile('alumnos.json', 'utf8', (err, data) => {
-        if (err)
-            throw err;
-        var arreglo = JSON.parse(data);
-        arreglo.push({nombre:"Alex Vargas"});
-        console.log(arreglo);
         
-       
-    
+        console.log(arreglo[posicion]);
+
     }); 
 }
 
-module.exports=leer;
+   //3-CONSULTA POR EMAIL
+    function BuscarElemento(posicion){
+         fs.readFile('alumnos.json', 'utf8', (err, data) => {
+             if (err)
+                 throw err;
+             var arreglo = JSON.parse(data);
+             
+             var newArr = arreglo.find(function(item) {
+            return item.email===posicion;
+
+             });
+             console.log(newArr);
+
+         }); 
+     }
+
+     //4-ELIMINAR POR EMAIL
+function eliminarxposicion(n){
+
+    fs.readFile('alumnos.json', (err, data) => {
+      if (err) throw err;
+      alumnos = JSON.parse(data);
+      alumnos.splice(n,1);
+      console.log(alumnos);
+      fs.writeFile('alumnos.json','['+JSON.stringify(alumnos)+']',(err)=>{
+        if(err)
+        throw err;
+        console.log("guardado");
+    });
+    });
+  
+}
+     //5-AGREGAR ELEMENTO
+     function AgregaElemento(nom,cor,gen,mat,esp,pro,fec,act,id){
+        var alumno = {
+            nombre : '',
+            email : '',
+            genero :'',
+            materias:[],
+            promedio:0,
+            fecha_nac:'',
+            activo:true
+        }
+        fs.readFile('alumnos.json', 'utf8', (err, data) => {
+            if (err)
+                throw err;
+             
+           //var stringified = JSON.stringify(data);
+           var arreglo = JSON.parse(data);
+           //arreglo.nombre=['Alex'];
+           alumno.nombre=nom;
+           alumno.email=cor;
+           alumno.genero=gen;
+           alumno.materias.push({nombre:'matematicas',calif:mat});
+           alumno.materias.push({nombre:'español',calif:esp});
+           alumno.promedio=pro;
+           alumno.fecha_nac=fec;
+           alumno.activo=act;
+           arreglo.push(alumno);
+           console.log(arreglo);
+          fs.writeFile('alumnos.json',JSON.stringify(arreglo),(err)=>{
+            if(err)
+            throw err;
+            console.log("guardado");
+        });
+      
+          
+        
+        }); 
+    }
+
+     //6-MODIFICAR POR EMAIL
+     function actualizar(nom,cor,gen,mat,esp,pro,fec,act,id){
+        var alumno = {
+            nombre : '',
+            email : '',
+            genero :'',
+            materias:[],
+            promedio:0,
+            fecha_nac:'',
+            activo:true
+        }
+        
+        fs.readFile('alumnos.json', 'utf8', (err, data) => {
+            if (err)
+                throw err;
+                alumno.nombre=nom;
+                alumno.email=id;
+                alumno.genero=gen;
+                alumno.materias.push({nombre:'matematicas',calif:mat});
+                alumno.materias.push({nombre:'español',calif:esp});
+                alumno.promedio=pro;
+                alumno.fecha_nac=fec;
+                alumno.activo=act;
+                var arreglo = JSON.parse(data);
+               var newArr = arreglo.find(function(item) {
+                    return item.email===id;
+     
+                     });
+                   
+                if(arreglo.indexOf(newArr===-1)){
+                    arreglo.splice(arreglo.indexOf(newArr),1,alumno);
+                 
+                    console.log("Actualizacion correcta");
+                    console.log(arreglo);
+                    fs.writeFile('alumnos.json',JSON.stringify(arreglo),(err)=>{
+                        if(err)
+                        throw err;
+                        console.log("guardado");
+                    });
+                }else if(arreglo.indexOf(newArr)>-1){
+                    console.log("ya existe");
+                }
+            
+               
+              
+               
+     
+        });
+     }
+
+
+module.exports.leer=leer;
+module.exports.leerXposicion=leerXposicion;
+module.exports.eliminarxposicion=eliminarxposicion;
 module.exports.BuscarElemento=BuscarElemento;
-//module.exports.AgregaElemento=AgregaElemento;
+module.exports.actualizar=actualizar;
+module.exports.AgregaElemento=AgregaElemento;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
-var index =require('./funciones').BuscarElemento;
+var index =require('./funciones.js').leer;
+//var myleer = index('Ulises Silva','email.com','femenino',50,60,70,'10/10/1910','true','email.com');
 var myleer = index();
